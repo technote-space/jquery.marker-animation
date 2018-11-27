c3d5b176819a60786cdd71cc56bc1d0b7f8e06a7
technote-space <technote.space@gmail.com>
2018-11-27 18:16:43 +0900

    依存ライブラリのバージョン更新等

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,10 @@ const webpackConfig = {
         'libraryTarget': 'umd'
     },
     'module': {
-        'loaders': [{
+        'rules': [{
             'test': /\.js$/,
             'exclude': /node_modules/,
-            'loader': 'babel'
+            'loader': 'babel-loader'
         }]
     },
     externals: {
