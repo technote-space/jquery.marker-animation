21b7dcaf2ecd14ae9aba3572cf85ba00b83c495b
Technote <technote.space@gmail.com>
2019-06-18 00:13:39 +0900

    #27 refactoring: divide files

---
+++ src/defaults.js
@@ -0,0 +1,15 @@
+export const SETTINGS_DEFAULTS = {
+	color: '#fe9',
+	paddingBottom: '.6em',
+	thickness: '.6em',
+	delay: '.1s',
+	duration: '2s',
+	function: 'ease',
+	fontWeight: 'bold',
+	repeat: false,
+	stripe: false,
+	stripeThickness: 2,
+	cssFilter: function( css ) {
+		return css;
+	},
+};
